79909f8fa22520dd15a197fdfcb3d93f19740a28
makuta <wangbo@tvapk.net>
2016-10-11 11:18:12 +0800

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp'),
-  clean = require('gulp-clean'),
-  sourcemaps = require('gulp-sourcemaps'),
-  gulpSequence = require('gulp-sequence'),
-  rollup = require('gulp-rollup'),
-  rollupConfig = require('./rollup.config');
+    clean = require('gulp-clean'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass');
+    gulpSequence = require('gulp-sequence'),
+    rollup = require('gulp-rollup'),
+    rename = require('gulp-rename'),
+    rollupConfig = require('./rollup.config');
+
 
 var Asset = {
-  js:'src/pages/**/**.js',
-  wxss:'src/**/**.wxss',
-  wxml:'src/**/**.wxml',
-  json:'src/**/**.json'
+    js: 'src/pages/**/**.js',
+    sass: 'src/**/**.scss',
+    wxml: 'src/**/**.wxml',
+    json: 'src/**/**.json'
 }
 
 //clean dist files
@@ -17,19 +20,18 @@ gulp.task('clean', function() {
   return gulp.src('dist').pipe(clean());
 });
 
+//copy files
 gulp.task('copy', function() {
   return gulp.src('src/*').pipe(gulp.dest('dist/'));
 });
 
 //parseJs
-gulp.task('parseJs', function () {
-  return gulp.src([
-    Asset.js,
-  ])
-  .pipe(sourcemaps.init())
-  .pipe(rollup(rollupConfig))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('dist/pages'));
+gulp.task('parseJs', function() {
+  return gulp.src([Asset.js, ]).
+    pipe(sourcemaps.init()).
+    pipe(rollup(rollupConfig)).
+    pipe(sourcemaps.write('.')).
+    pipe(gulp.dest('dist/pages'));
 })
 
 //parseWxml
@@ -37,9 +39,14 @@ gulp.task('parseWxml', function() {
   return gulp.src(Asset.wxml).pipe(gulp.dest('dist/'));
 });
 
-//parseWxss
-gulp.task('parseWxss', function() {
-  return gulp.src(Asset.wxss).pipe(gulp.dest('dist/'));
+//parseSass
+gulp.task('parseSass', function() {
+  return gulp.src(Asset.sass).
+    pipe(sass().on('error', sass.logError)).
+    pipe(rename(function(path) {
+      path.extname = ".wxss"
+    })).
+    pipe(gulp.dest('dist/'));
 });
 
 //parseJson
@@ -47,12 +54,13 @@ gulp.task('parseJson', function() {
   return gulp.src(Asset.json).pipe(gulp.dest('dist/'));
 });
 
-gulp.task('watch',function(){
+//watch
+gulp.task('watch', function() {
   gulp.watch(Asset.js, ['parseJs']);
-  gulp.watch(Asset.wxss, ['parseWxss']);
+  gulp.watch(Asset.sass, ['parseSass']);
   gulp.watch(Asset.wxml, ['parseWxml']);
   gulp.watch(Asset.json, ['parseJson']);
 });
 
 //build
-gulp.task('build', gulpSequence('clean','copy', 'parseJs','parseJson','parseWxml','parseWxss'));
+gulp.task('build', gulpSequence('clean', 'copy', 'parseJs', 'parseJson','parseWxml', 'parseSass'));
